de5e03e5573e9318a58386cec0f8e204dfb2ce2a
Naman Gupta <namangupta@Namans-MacBook-Air-3.local>
2024-02-05 22:13:16 +0530

    Initial Commit

--- chatbot1.py
+++ chatbot1.py
@@ -1,103 +1,91 @@
-#This is a corona disease helpline/information chat box
-#pip install newspaper3k
-#or pip install newspaper
+# Imports
 from newspaper import Article
 import random
 import string
 import nltk
+import os
 from sklearn.feature_extraction.text import CountVectorizer
-#eg: "I am a student of graphic era university, I am in 6th sem. I live in Kanpur......" for questions like : "Who are you"
 from sklearn.metrics.pairwise import cosine_similarity
-#1.0 -> 100% match 0-> 0% match
-import numpy as np
-import warnings
-# warneings.filterwarnings('ignore') to ignore warinings
 
-#Download the punkt package
-nltk.download('punkt',quiet=True)
+nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
+nltk.data.path.append(os.path.abspath(nltk_data_path))
 
-#get the article
-article= Article('https://en.wikipedia.org/wiki/Coronavirus')
-article.download()
-article.parse()
-article.nlp()
-corpu = article.text
-article= Article('https://en.wikipedia.org/wiki/BTS')
-article.download()
-article.parse()
-article.nlp()
-corpu = corpu+' '+(article.text)
-#print the article text
-#print(corpu)
+# Download the punkt package
+nltk.download('punkt', quiet=True)
 
-#tokennisation
-test= corpu
-sentence_list=nltk.sent_tokenize(test)
-#A list of sentences will be produced
-#print(sentence_list)
+# Function to get and parse articles
+def get_parsed_articles(urls):
+    corpus = ''
+    for url in urls:
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        corpus += article.text + ' '
+    return corpus
 
-#A function to return a randon greeting message to a user
+# Function for greeting response
 def greet_response(text):
-    text= text.lower() #hello, HELLO, Hello
-    bot_greet=['hello','hi','hey','wassup']
-    user_greet=['hi','hello','helo','hiii','wassup','hey']
-    #try using as many words as possible
-    
+    text = text.lower()
+    bot_greet = ['hello', 'hi', 'hey', 'wassup']
+    user_greet = ['hi', 'hello', 'helo', 'hiii', 'wassup', 'hey']
     for word in text.split():
         if word in user_greet:
             return random.choice(bot_greet)
 
-#index sort function
+# Function to sort indices based on values
 def index_sort(list_var):
-    length=len(list_var)
-    list_index=list(range(0,length))
-    x=list_var
+    length = len(list_var)
+    list_index = list(range(length))
+    x = list_var
     for i in range(length):
         for j in range(length):
-            if x[list_index[i]]>x[list_index[j]]:
-                #swap
-                temp=list_index[i]
-                list_index[i]=list_index[j]
-                list_index[j]=temp
-    
+            if x[list_index[i]] > x[list_index[j]]:
+                temp = list_index[i]
+                list_index[i] = list_index[j]
+                list_index[j] = temp
     return list_index
 
-#Bot response to actual queries
+# Function for bot response to queries
 def bot_response(user_input):
-    #main function to response queries
-    user_input=user_input.lower()
+    user_input = user_input.lower()
     sentence_list.append(user_input)
-    bot_res=''
+    bot_res = ''
     cm = CountVectorizer().fit_transform(sentence_list)
-    similarity_score=cosine_similarity(cm[-1],cm)
-    similarity_score_list=similarity_score.flatten()
-    #0.0, 0.4, 0.6, 0.2, 1.0, 0.9
-    index=index_sort(similarity_score_list)
-    index=index[1:]
-    response_flag=0
-    j=0
+    similarity_score = cosine_similarity(cm[-1], cm)
+    similarity_score_list = similarity_score.flatten()
+    index = index_sort(similarity_score_list)
+    index = index[1:]
+    response_flag = 0
+    j = 0
     for i in range(len(index)):
-        if similarity_score_list[index[i]]>0.001:
-            bot_res=bot_res+' '+sentence_list[index[i]]
-            response_flag=1
-            j=j+1
-        if j>2:
+        if similarity_score_list[index[i]] > 0.001:
+            bot_res = bot_res + ' ' + sentence_list[index[i]]
+            response_flag = 1
+            j += 1
+        if j > 2:
             break
-    if response_flag==0:
-        bot_res= bot_res + 'I apologise, that I have not understood you meaning.'
+    if response_flag == 0:
+        bot_res = bot_res + 'I apologize, I have not understood your meaning.'
     sentence_list.remove(user_input)
     return bot_res
 
-#start the chat
-print("Covid Helpline: I am here to help you with the inofomation regarding Covid virus. If you want to exit type bye or exit.")
-exit_list=['bye','exit','byee','break','quit']
-while(True):
-    user_input=input()
+# Start the chat
+print("Covid Helpline: I am here to help you with information regarding the Covid virus. If you want to exit, type bye or exit.")
+exit_list = ['bye', 'exit', 'byee', 'break', 'quit']
+
+# URLs for articles
+article_urls = ['https://en.wikipedia.org/wiki/Coronavirus', 'https://en.wikipedia.org/wiki/BTS']
+corpus = get_parsed_articles(article_urls)
+sentence_list = nltk.sent_tokenize(corpus)
+
+while True:
+    user_input = input()
     if user_input.lower() in exit_list:
         print("Bot: Thank you for connecting with us. See you later.")
         break
     else:
-        if greet_response(user_input) != None:
-            print('Bot: '+greet_response(user_input))
+        if greet_response(user_input) is not None:
+            print('Bot: ' + greet_response(user_input))
         else:
-            print('Bot: '+bot_response(user_input))
+            print('Bot: ' + bot_response(user_input))
